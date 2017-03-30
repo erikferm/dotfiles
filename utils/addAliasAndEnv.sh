1e2028ddb6a89b70cc7a45786839425a1b8ea230
ErikFerm <erik.ferm@tv4.se>
2017-03-30 23:00:12 +0200

    Only add aliases and env if file exists

--- utils/addAliasAndEnv.sh
+++ utils/addAliasAndEnv.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 HERE=$1
 ## Add nvim aliases
-ALIASES=$(cat $HERE/aliases)
-for line in $ALIASES
-do
-	ALIASES_SET=$(cat $HOME/.aliases | grep "$line")
-	if [ -z "$ALIASES_SET" ]; then
-		echo "$ALIASES" >> $HOME/.aliases
-	fi
-	ALIASES_SET=""
-done
+if [ -f $HERE/aliases ]; then
+	
+	ALIASES=$(cat $HERE/aliases)
+	for line in $ALIASES
+	do
+		ALIASES_SET=$(cat $HOME/.aliases | grep "$line")
+		if [ -z "$ALIASES_SET" ]; then
+			echo "$ALIASES" >> $HOME/.aliases
+		fi
+		ALIASES_SET=""
+	done
+fi
 
 ## Add nvim envvars
-ENVVARS=$(cat $HERE/envvars)
-for line in $ENVVARS
-do
-	ENVVARS_SET=$(cat $HOME/.envvars | grep "$line")
-	if [ -z "$ENVVARS_SET" ]; then
-		echo "$ENVVARS" >> $HOME/.envvars
-	fi
-	ENVVARS_SET=""
-done
+if [ -f $HERE/envvars ]; then
+
+	ENVVARS=$(cat $HERE/envvars)
+	for line in $ENVVARS
+	do
+		ENVVARS_SET=$(cat $HOME/.envvars | grep "$line")
+		if [ -z "$ENVVARS_SET" ]; then
+			echo "$ENVVARS" >> $HOME/.envvars
+		fi
+		ENVVARS_SET=""
+	done
+fi
 
 source $HOME/.aliases
 source $HOME/.envvars
