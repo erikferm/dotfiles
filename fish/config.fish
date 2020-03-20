f2343e30003204d3c318346f724e9376277c441e
ErikFerm <7500445+erikferm@users.noreply.github.com>
2020-03-20 22:24:13 +0100

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,12 @@ set fish_color_git_branch "d7af00"
 set fish_color_user "5f8787"
 
 # Git config
+# https://github.com/fish-shell/fish-shell/blob/master/share/functions/fish_git_prompt.fish
 set -xg __fish_git_prompt_show_informative_status true
-set -xg __fish_git_prompt_showdirtystate true
-set -xg __fish_git_prompt_showcolorhints true
-set -xg __fish_git_prompt_use_informative_chars true
-set -xg __fish_git_prompt_char_dirtystate x
+set -xg __fish_git_prompt_char_dirtystate "✗"
+set -xg __fish_git_prompt_char_untrackedfiles "✗"
+set -xg __fish_git_prompt_char_stateseparator ""
+set -xg __fish_git_prompt_color_flags --bold red
+set -xg __fish_git_prompt_color_branch --bold "d7af00"
+set -xg __fish_git_prompt_color_prefix --bold "5f8787"
+set -xg __fish_git_prompt_color_suffix --bold "5f8787"
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,6 +1,7 @@
 function fish_prompt
   set curr_dir (pwd)
-  printf '%s%s~> ' \
-    (set_color $fish_color_user && basename $curr_dir) \
+  printf '%s/%s%s $ ' \
+    (set_color --bold $fish_color_user && basename $curr_dir) \
+    (set_color normal) \
     (fish_git_prompt)
 end
