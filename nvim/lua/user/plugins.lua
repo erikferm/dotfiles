2323730624136b89a4e18ac6604bac7104e3e973
erikferm <erik.b.ferm@gmail.com>
2022-04-19 12:48:18 +0200

    Add bootstrap for new installations

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,5 +1,10 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
 require('packer').startup(
-  function()
+  function(use)
     use 'wbthomason/packer.nvim'
     use 'NLKNguyen/papercolor-theme'
     use 'rebelot/kanagawa.nvim'
@@ -34,6 +39,9 @@ require('packer').startup(
       }
     }
     use { "nvim-telescope/telescope-file-browser.nvim" }
+    if packer_bootstrap then
+      require('packer').sync()
+    end
   end
 )
 
