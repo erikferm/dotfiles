c9893de6958c35925263b9bad9afb7e28050a2a4
erikferm <erik.b.ferm@gmail.com>
2022-08-09 15:55:56 +0200

    Add CFLAGS envvar

--- fish/config.fish
+++ fish/config.fish
@@ -34,6 +34,7 @@ set -x LC_CTYPE en_US.UTF-8
 ## CPATH
 set -x LIBRARY_PATH (brew --prefix)/lib
 set -x C_INCLUDE_PATH (brew --prefix)/include
+set -ax CFLAGS "-I$C_INCLUDE_PATH -L$LIBRARY_PATH"
 
 ## LDFlags
 #set -x LDFLAGS "-L"(brew --prefix)"/lib"
