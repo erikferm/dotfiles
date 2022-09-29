ac27393bc0f19f13c6ee31e7a83cc0084e32472a
erikferm <erik.b.ferm@gmail.com>
2022-09-29 09:27:39 +0200

    Change the prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,5 +2,5 @@ function fish_prompt
   set curr_dir (pwd)
   printf '%s' \
     (set_color --bold $fish_color_dir && basename $curr_dir)'/' \
-    (fish_git_prompt) (set_color $fish_color_char) ' 字 '
+    (fish_git_prompt)\n (set_color $fish_color_char) '字> '
 end
