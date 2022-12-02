bb9593eeda8428f5effb06d755ec7d9ae941cea0
erikferm <erik.b.ferm@gmail.com>
2022-12-02 09:17:12 +0100

    Remove c-k mapping and update capabilities

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -4,19 +4,8 @@ local pyright = require('user.lsp.pyright')
 local yaml = require('user.lsp.yaml')
 local terraform = require('user.lsp.terraform')
 
---local lsp_installer = require('nvim-lsp-installer')
---lsp_installer.on_server_ready(
---  function(server)
---    local opts = {}
---    if server.name == 'sumneko_lua' then
---      opts = lua_server.opts
---    end
---    server:setup(opts)
---  end
---)
-
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
@@ -30,7 +19,6 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', 'gr', ':lua vim.lsp.buf.references()<cr>', opts)
   buf_set_keymap('n', 'gt', ':lua vim.lsp.buf.type_definition()<cr>', opts)
   buf_set_keymap('n', 'K', ':lua vim.lsp.buf.hover()<cr>', opts)
-  buf_set_keymap('n', '<c-k>', ':lua vim.lsp.buf.signature_help()<cr>', opts)
   buf_set_keymap('n', '<leader>af', ':lua vim.lsp.buf.code_action()<cr>', opts)
   buf_set_keymap('n', '<leader>rn', ':lua vim.lsp.buf.rename()<cr>', opts)
   if client.server_capabilities.document_formatting then
