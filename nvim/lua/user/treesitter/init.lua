4deaf00616923f720e0627e561db396858d8f6a9
erikferm <erik.b.ferm@gmail.com>
2022-12-12 18:44:09 +0100

    Add treesitter config

--- nvim/lua/user/treesitter/init.lua
+++ nvim/lua/user/treesitter/init.lua
@@ -1,12 +1,12 @@
---local configs = require('nvim-treesitter.configs')
---configs.setup {
---  ensure_installed = "maintained",
---  highlight = {
---    enable = true,
---    disable = {},
---  },
---  indent = {
---    enable = false,
---    disable = {},
---  }
---}
+local configs = require('nvim-treesitter.configs')
+configs.setup {
+  ensure_installed = {"hcl","go","lua","rust","python"},
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = false,
+    disable = {},
+  }
+}
