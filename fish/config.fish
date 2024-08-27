1d13f3a15f1d8cd718561a08822a853efc7fe99b
erikferm <erik.b.ferm@gmail.com>
2024-08-27 09:52:36 +0200

    Move other brew specific stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,22 +1,26 @@
 ## Fish configuration
 source ~/.config/fish/themes/kanagawa.fish
 
-set arch (arch)
-
 # OSX specific setup (mostly brew stuff)
 if test (uname) = "Darwin"
+  set arch (arch)
   if [ "$arch" = "arm64" ]
     echo "AAAARM"
     eval (/opt/homebrew/bin/brew shellenv)
   else if [ "$arch" = "i386" ]
     echo "INTEEEEL"
     eval (/usr/local/bin/brew shellenv)
-  end 
+  end
   # Add brew installations to path
   set -ax PATH (brew --prefix)/bin
   ## Java stuff
   #
   set -x JAVA_HOME (brew --prefix)'/opt/openjdk@11/libexec/openjdk.jdk/Contents/Home/'
+
+  ## CPATH
+  set -x LIBRARY_PATH (brew --prefix)/lib
+  set -x C_INCLUDE_PATH (brew --prefix)/include
+  set -ax CFLAGS "-I$C_INCLUDE_PATH -L$LIBRARY_PATH"
 end
 
 ## CDPath
@@ -49,11 +53,6 @@ set -ax PATH $CARGO_HOME/bin
 set -x LANG en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
-
-## CPATH
-set -x LIBRARY_PATH (brew --prefix)/lib
-set -x C_INCLUDE_PATH (brew --prefix)/include
-set -ax CFLAGS "-I$C_INCLUDE_PATH -L$LIBRARY_PATH"
 
 ## If gcloud is installed
 if type -q gcloud
