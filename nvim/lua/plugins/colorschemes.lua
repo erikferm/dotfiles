2406b1593e7051abd4ff08959e3ceb916e246abb
Erik Ferm <erik@dbt.se>
2026-01-19 21:28:18 +0100

    Use wave for darkmode

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,16 +1,16 @@
 return {
-	{
+  {
     'rebelot/kanagawa.nvim',
     lazy = true,
     priority = 1000,
     opts = {
       theme = "wave",
       background = {
-        dark = "dragon",
+        dark = "wave",
         light = "lotus"
       }
     },
-	},
+  },
   {
     "folke/tokyonight.nvim",
     priority = 1000,
